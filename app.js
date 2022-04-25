89a36a15cd4e3dcd61b9402dd0ab6884bf7ae0b9
MBMomen <you@example.com>
2022-04-25 23:07:33 +0200

    Implemented the stiky navbar behavior and added some comments

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
@@ -11,17 +8,23 @@ const btnScrollTo = document.querySelector(`.btn--scroll-to`);
 const section1 = document.querySelector(`#section--1`);
 const navbar = document.querySelector(`.nav`);
 const links = navbar.querySelectorAll(`.nav__link`);
-//Scroll to section corresponding to the nav link
-navbar.addEventListener(`click`, (e) => {
-  e.preventDefault();
-  const target = e.target.classList.contains(`nav__link`) ? e.target : false;
+const header = document.querySelector(`.header`);
 
-  target &&
-    document
-      .querySelector(`${target.getAttribute(`href`)}`)
-      .scrollIntoView({ behavior: `smooth` });
-});
+const navbarHeight = navbar.getBoundingClientRect().height; // Calculating the nav height
 
+//////////////////////////////////////////////////
+// Implementing scrolling to view  >for the first
+// sectin only function
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-//
+// Scrolling to the first section by pressing read more link/btn
+btnScrollTo.addEventListener(`click`, () =>
+  section1.scrollIntoView({ behavior: `smooth` })
+);
+
+//////////////////////////////////////////////////
+// Implementing the Hovering effect to the nav
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-//
+//Handel the hovering effect for the nav links
 const navHoverHandeler = function (e) {
   const target = e.target.classList.contains(`nav__link`) ? e.target : false;
 
@@ -34,16 +37,51 @@ const navHoverHandeler = function (e) {
   }
 };
 
+//hovring nav links change style event
 navbar.addEventListener(`mouseover`, navHoverHandeler.bind(0.5));
-
+//bit hovring nav links reset style event
 navbar.addEventListener(`mouseout`, navHoverHandeler.bind(1));
 
-// Scrolling to the first section by pressing read more link/btn
-btnScrollTo.addEventListener(`click`, () =>
-  section1.scrollIntoView({ behavior: `smooth` })
-);
+//////////////////////////////////////////////////
+// Implementing dynamic scrollToView nav links
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-//
+//Scroll to section corresponding to the nav link
+navbar.addEventListener(`click`, (e) => {
+  e.preventDefault();
+  const target = e.target.classList.contains(`nav__link`) ? e.target : false;
+  const id = target.getAttribute(`href`);
+  target && document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+});
 
+//////////////////////////////////////////////////
+// Implementing the sticky nav function
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-//
+// Togaling sticky bar class
+const stickyNavHandler = function (entries) {
+  const [entry] = entries; // the observer trigger data
+  console.log(entry);
+  if (!entry.isIntersecting) navbar.classList.add(`sticky`);
+  else navbar.classList.remove(`sticky`);
+};
+
+// The observer options
+const obsOptions = {
+  root: null, //Null meaning we tracking the intersection with Window (viewport)
+  threshold: 0,
+  rootMargin: `-${navbarHeight}px`,
+};
+
+// Creating a observer to track the intersaction between our target (header)
+// and the element it should intersect with (Window/Viewport)
+const headerObserver = new IntersectionObserver(stickyNavHandler, obsOptions);
+
+// Initilizing the observer with the header as a target element
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Modal window
 //Create account modal pop up
+
 //open modal
 const openModal = function () {
   modal.classList.remove("hidden");
