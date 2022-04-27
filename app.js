a8d9a12f8b2ddd433a20ba8f22ea4ffc9821ef28
MBMomen <you@example.com>
2022-04-27 17:46:55 +0200

    Added revealing section while scrolling effect

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const header = document.querySelector(`.header`);
 const tabContainer = document.querySelector(`.operations`);
 const tabBtns = document.querySelectorAll(`.operations__tab`);
 const tabsContent = document.querySelectorAll(`.operations__content`);
+const sections = document.querySelectorAll(`.section`);
 
 const navbarHeight = navbar.getBoundingClientRect().height; // Calculating the nav height
 
@@ -62,7 +63,7 @@ navbar.addEventListener(`click`, (e) => {
 // Togaling sticky bar class
 const stickyNavHandler = function (entries) {
   const [entry] = entries; // the observer trigger data
-  console.log(entry);
+
   if (!entry.isIntersecting) navbar.classList.add(`sticky`);
   else navbar.classList.remove(`sticky`);
 };
@@ -97,6 +98,37 @@ tabContainer.addEventListener(`click`, (e) => {
   document
     .querySelector(`.operations__content--${target.dataset.tab}`)
     .classList.add(`operations__content--active`); //adding the active class to the content that corresponds to the clicked button
+});
+
+//////////////////////////////////////////////////
+// Reveal Section while scrolling funcrionality
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-//
+//Revealing the section handler as a callback
+//function for the observer
+const showSection = function (entries, observer) {
+  const [entry] = entries; //Getting the observer data
+
+  if (!entry.isIntersecting) return; //Guerd cluase to stop revealing section 1 while not intersacting
+
+  entry.target.classList.remove(`section--hidden`); // revealing the section by removing the hidden class
+  observer.unobserve(entry.target); // stoping the observer for tow resons
+  // 1- We don't need it anymore since we show the section only on time
+  // 2- This is also better performance-wise
+};
+
+// The observer properties
+const showOptions = {
+  root: null, //intersecting with the main window (Veiwport)
+  threshold: 0.15, //Trigger the observer entry when the section intersect by 15%
+};
+
+//making the intersection Observer
+const sectionsObserver = new IntersectionObserver(showSection, showOptions);
+
+//looping through the section to call the observer and add the hidden class dynamically
+sections.forEach((section) => {
+  sectionsObserver.observe(section); // Calling the observer in to each section
+  section.classList.add(`section--hidden`); //adding the hiding class dynamically
 });
 
 /////////////////////////////////////////////////
